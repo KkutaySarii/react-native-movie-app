badf380259d74041cfd8c4585f200a80d960e167
KkutaySarii <h.kutay.1@gmail.com>
2025-05-05 13:04:21 +0300

    removed env

---
+++ services/useFetch.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err : new Error("An unknown error occurred")
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetchFunction, autoFetch]);
+
+  return { data, error, loading, refetch: fetchData, reset };
+};
+
+export default useFetch;
+// Usage example
+// const { data, error, loading, refetch } = useFetch(() => fetchMovies({ query }), false);
